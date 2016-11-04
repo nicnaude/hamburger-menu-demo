370add5e75ad6df1b964a8da2ee164c8b8ff8c78
Nicholas Naudé <nicnaude@gmail.com>
2016-11-03 19:10:26 -0700

    Initial Commit

---
+++ Week 4 Lab: Youtube Hamburger MenuUITests/Week_4_Lab__Youtube_Hamburger_MenuUITests.swift
@@ -0,0 +1,36 @@
+//
+//  Week_4_Lab__Youtube_Hamburger_MenuUITests.swift
+//  Week 4 Lab: Youtube Hamburger MenuUITests
+//
+//  Created by Nicholas Naudé on 03/11/2016.
+//  Copyright © 2016 Nicholas Naudé. All rights reserved.
+//
+
+import XCTest
+
+class Week_4_Lab__Youtube_Hamburger_MenuUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
